8c976a7649d3f647b293ecc955c03df97d1a5240
Gabrielle Ohlson <62773565+gabrielle-ohlson@users.noreply.github.com>
2021-04-26 15:39:21 -0400

    Update prism-knitout.js

--- components/prism-knitout.js
+++ components/prism-knitout.js
@@ -1,1 +1,42 @@
-
+Prism.languages.knitout = {
+  header: [
+    {
+      pattern: /(^|[^\\:]);!.*/,
+      lookbehind: !0,
+      greedy: !0,
+    },
+    {
+      pattern: /(^|[^\\:]);;.*/,
+      lookbehind: !0,
+      greedy: !0,
+    },
+  ],
+  comment: [
+    {
+      pattern: /(^|[^\\:]);.*/,
+      lookbehind: !0,
+      greedy: !0,
+    },
+  ],
+  extension: [
+    {
+      pattern: /x-(.*?)[\s]/,
+    },
+  ],
+  knit: /knit/g,
+  xfer: /xfer/g,
+  tuck: /tuck/g,
+  miss: /miss/g,
+  drop: /drop/g,
+  rack: [
+    {
+      pattern: /rack(.*?)[\s]/,
+    },
+  ],
+  hook: /inhook|releasehook|outhook|in |out /,
+  direction: / - | \+ /g,
+  carrier: / \d\s| \d;*/,
+  bed_f: /f+(?=\d)/,
+  bed_b: /b+(?=\d)/,
+  kn_number: /^\d+$/,
+};
